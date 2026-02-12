00e62de12983467fbfb5400e261b6f9b8798158d
BuuurnZ <florian91800@gmail.com>
2026-02-12 14:48:14 +0100

    implementation classe Carte

--- carte.py
+++ carte.py
@@ -1,1 +1,32 @@
 # classe pour representer une carte de poker
+
+
+VALEURS_FIGURES = {
+    "J": 11,
+    "Q": 12,
+    "K": 13,
+    "A": 14,
+}
+
+
+class Carte:
+    def __init__(self, rang, couleur):
+        self.rang = rang
+        self.couleur = couleur
+
+    def valeur(self):
+        if self.rang in VALEURS_FIGURES:
+            return VALEURS_FIGURES[self.rang]
+        return int(self.rang)
+
+    def __eq__(self, autre):
+        return self.rang == autre.rang and self.couleur == autre.couleur
+
+    def __repr__(self):
+        return f"{self.rang} de {self.couleur}"
+
+    def __str__(self):
+        return f"{self.rang} de {self.couleur}"
+
+    def __hash__(self):
+        return hash((self.rang, self.couleur))
